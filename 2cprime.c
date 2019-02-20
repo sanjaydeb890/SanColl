1f2993a982fede0de1073f7c034751a1ca9164fb
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-02-20 16:18:33 +0530

    Create 2cprime.c

---
+++ 2cprime.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i;
+printf("Enter number to check");
+scanf("%d",&num);
+for(i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+if(num==1)
+{
+Printf("Either prime or composite");
+}
+else{
+if(flag==0)
+{
+printf("Yes");
+}
+else
+{
+printf("No");
+}
+}
+getch();
+}
