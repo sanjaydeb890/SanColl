f41dbe9851a604e2b6a19def488e193c1fb44917
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-02-21 16:31:20 +0530

    Create 2ifactorial.c

---
+++ 2ifactorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,fact=1;
+printf("Insert number"):
+scanf("%d",&i);
+for(j=1;j<=i;j++)
+{
+fact=fact*j;
+}
+printf("%d",fact);
+getch()
+}
