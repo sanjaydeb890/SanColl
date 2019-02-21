9b21dbd5b9770a3a07962d29a1354ac4cab6667e
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-02-21 16:13:22 +0530

    Create 2gcheckarmstrong.c

---
+++ 2gcheckarmstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,org,rem,result=0;
+printf("Enter the number");
+scanf("%d",&num);
+org=num;
+while(org!=0)
+{
+rem=org%10;
+result+=rem*rem*rem;
+org/=10;
+}
+if(org==num)
+{
+printf("Yes");
+}
+else
+{
+printf("No');
+}
+getch();
+}
