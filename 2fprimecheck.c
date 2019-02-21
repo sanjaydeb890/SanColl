407d521355af07f96855c4d6eff1ab29f617be0a
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-02-21 15:53:37 +0530

    Create 2fprimecheck.c

---
+++ 2fprimecheck.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int start,end,flag;
+printf("Enter interval"):
+scanf("%d%d",&start,&end);
+while(start<end)
+{
+flag=0;
+for(i=2;i<=start/2;i++)
+{
+if(start%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==1)
+{
+printf("%d ",start);
+}
+++start;
+}
+getch();
+}
