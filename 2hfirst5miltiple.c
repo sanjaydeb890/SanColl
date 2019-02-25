12ec2686014e2550d85e649a145052094546d556
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-02-25 16:01:53 +0530

    Create 2hfirst5miltiple.c

---
+++ 2hfirst5miltiple.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i,res=1;
+printf("Enter number");
+scanf("%d",&num);
+for(i=1;i>=5;i++)
+{
+res=5*i;
+printf("%d",res);
+res=1;
+}
+getch();
+}
