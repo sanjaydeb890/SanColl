a5eb50d64046782c24b5dbd4c430a76b1cc5a89d
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-02-25 16:21:25 +0530

    Create 1set3.c

---
+++ 1set3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int size,arr[20],i,large;
+printf("Enter the size of array");
+scanf("%d",&size);
+printf("Enter the array element");
+for(i=0;i<=size;i++)
+{
+scanf("%d",arr[i]);
+}
+large=arr[0];
+for(i=0;i<=size;i++)
+{
+if(large<arr[i])
+{
+large==arr[i];
+}
+}
+printf("%d",large);
+getch();
+}
