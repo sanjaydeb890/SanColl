ca5f0e8f5e662764d8d866999ea3e54d1dfca190
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-03-25 08:10:21 +0530

    Create 3.5 Medianelement.c

---
+++ 3.5 Medianelement.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void) {
+int a[100],given,i,j,temp;
+int median=0;
+scanf("%d",&given);
+for(i=0;i<given;i++)
+scanf("%d",&a[i]);
+for(i=0;i<given;i++)
+{
+for(j=i+1;j<given;j++)
+{
+if(a[j]<a[i])
+{
+temp=a[j];
+a[j]=a[i];
+a[i]=temp;
+}
+}
+}
+if(given%2==0) {
+median=((a[given/2] + a[given/2 - 1]) / 2.0);
+printf("%d",median);
+} else { 
+median=a[given/2];
+printf("%d",median);
+}     
+return 0;
+}
