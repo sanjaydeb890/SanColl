7173fba3fad657fc2e5a10863d051e38f7b6b311
sanjaydeb890 <47547305+sanjaydeb890@users.noreply.github.com>
2019-03-27 10:55:13 +0530

    Create 4.4 noofspecialchar.c

---
+++ 4.4 noofspecialchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+char str[100],i,c=0,s;
+printf("enter a character");
+for(s=0;s<=100;s++)
+{
+scanf("%c",&str[s]);
+}
+for(i=0;str[i]!='\0';i++)
+{
+if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')||(str[i] >= '0' && str[i] <= '9'))
+{
+ }
+else
+{
+c++;
+}
+}
+printf("%d",c);
+getch();
+}
